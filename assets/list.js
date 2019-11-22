7eb5ef235693e37848c132e2830da81a089178e1
ilan-gold <ilanbassgold@gmail.com>
2019-11-22 15:54:09 -0500

    Use JQuery for JS list manipulation

--- assets/list.js
+++ assets/list.js
@@ -7,42 +7,24 @@ function makeUL(array) {
     })
     return list;
 }
-// Get list of events.
-var eventList = Array.from(document.getElementsByClassName('event-link'));
 var now = new Date();
 // Filter to get upcoming list.
-var upcomingList = eventList.filter((a) => new Date(a.attributes.date.value.split(' ')[0] + ' ' + a.attributes.date.value.split(' ')[1]) >= new Date());
-var eventDiv = document.getElementById('event-list');
-var upcomingEventDiv = document.getElementById('upcoming-event');
+var eventLists = document.getElementById('event-lists');
+var homeEvents = document.getElementById('home-events');
 // This is the home page.
-if (upcomingEventDiv != undefined) {
-  upcomingEventDiv.innerHTML = '';
-  upcomingEventDiv.appendChild(upcomingList[0]);
+if (homeEvents != undefined) {
+    $('#home-events [data-date]').filter(function(){
+      return (new Date($(this).data('date').split(' ')[0] +  ' ' + $(this).data('date').split(' ')[1]) < now);
+    }).remove()
+    $('#home-events [data-date]').slice(1).remove()
 }
 // This is the events page.
-else if(eventDiv != undefined){
-  // Set innerHTML to be empty.
-  eventDiv.innerHTML = '';
-
-  // Create upcoming list html and append.
-  var upcomingDiv = document.createElement('div');
-  upcomingDiv.id = 'upcoming';
-  upcomingDiv.appendChild(makeUL(upcomingList));
-  var upcomingHeader = document.createElement('H3');
-  var text = document.createTextNode("Upcoming Calls");
-  upcomingHeader.appendChild(text);
-  eventDiv.appendChild(upcomingHeader);
-  eventDiv.appendChild(upcomingDiv);
-
-  // Filter to get past list.
-  var pastList = eventList.filter((a) => new Date(a.attributes.date.value.split(' ')[0] + ' ' + a.attributes.date.value.split(' ')[1]) < new Date()).reverse();
-  // Create past list html and append.
-  var pastDiv = document.createElement('div');
-  pastDiv.id = 'past';
-  pastDiv.appendChild(makeUL(pastList));
-  var pastHeader = document.createElement('H3');
-  text = document.createTextNode("Past Calls");
-  pastHeader.appendChild(text);
-  eventDiv.appendChild(pastHeader);
-  eventDiv.appendChild(pastDiv);
+else if(eventLists != undefined){
+  $('#upcoming [data-date]').filter(function(){
+      return (new Date($(this).data('date').split(' ')[0] + ' ' + $(this).data('date').split(' ')[1]) < now);
+    }).remove();
+  $('#past [data-date]').filter(function(){
+      return (new Date($(this).data('date').split(' ')[0] + ' ' + $(this).data('date').split(' ')[1]) >= now);
+    }).remove();
+  $('#past').children().each((e,i) => $('#past').prepend(i))
 }
--- events.markdown
+++ events.markdown
@@ -2,15 +2,29 @@
 layout: page
 title: Events
 ---
-<div id="event-list">
-<br/>
+<div id="event-lists">
+<h3>Upcoming Events</h3>
+<ul id='upcoming'>
 {%- for event in site.events -%}
-<div class="event-link" date="{{ event.date }}">
+<li class="event-link" data-date="{{ event.date }}">
 <a  href="{{ event.url | relative_url }}" >{{event.title}}</a>
 <br/>
 {{event.summary}}
 <br/>
 <br/>
-</div>                                   
+</li>                                   
 {%- endfor -%}
+</ul>
+<h3>Past Events</h3>
+<ul id='past'>
+{%- for event in site.events -%}
+<li class="event-link" data-date="{{ event.date }}">
+<a  href="{{ event.url | relative_url }}" >{{event.title}}</a>
+<br/>
+{{event.summary}}
+<br/>
+<br/>
+</li>                                   
+{%- endfor -%}
+</ul>
 </div>
