3b45fb2043713e4085f2789a35b50d0a9a1792b0
ilan-gold <ilanbassgold@gmail.com>
2019-11-22 16:31:33 -0500

    Make final changes

--- assets/list.js
+++ assets/list.js
@@ -1,21 +1,17 @@
+function asDate(el) {
+  return new Date(el.split(' ')[0] +  ' ' + el.split(' ')[1])
+}
 var now = new Date();
-// Filter to get upcoming list.
-var eventLists = document.getElementById('event-lists');
-var homeEvents = document.getElementById('home-events');
 // This is the home page.
-if (homeEvents != undefined) {
-    $('#home-events [data-date]').filter(function(){
-      return (new Date($(this).data('date').split(' ')[0] +  ' ' + $(this).data('date').split(' ')[1]) < now);
-    }).remove()
-    $('#home-events [data-date]').slice(1).remove()
-}
+$('#home-events [data-date]').filter(function(){
+  return (asDate($(this).data('date')) < now);
+}).remove()
+$('#home-events [data-date]').slice(1).remove()
 // This is the events page.
-else if(eventLists != undefined){
-  $('#upcoming [data-date]').filter(function(){
-      return (new Date($(this).data('date').split(' ')[0] + ' ' + $(this).data('date').split(' ')[1]) < now);
-    }).remove();
-  $('#past [data-date]').filter(function(){
-      return (new Date($(this).data('date').split(' ')[0] + ' ' + $(this).data('date').split(' ')[1]) >= now);
-    }).remove();
-  $('#past').children().each((e,i) => $('#past').prepend(i))
-}
+$('#upcoming [data-date]').filter(function(){
+    return (asDate($(this).data('date')) < now);
+  }).remove();
+$('#past [data-date]').filter(function(){
+    return (asDate($(this).data('date')) >= now);
+  }).remove();
+$('#past').children().each((e,i) => $('#past').prepend(i))
